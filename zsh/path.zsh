56778fcd9a053659ad1df0823294765bc5ac6428
Kei <kei-sato@meeetol.com>
2017-08-07 10:56:46 +1000

    set cuda home

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,6 +5,7 @@ export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
 
 # CUDA
 # https://github.com/pfnet/chainer
+export CUDA_HOME=/usr/local/cuda
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 export CPATH=/usr/local/cuda/include${CPATH:+:$CPATH}
