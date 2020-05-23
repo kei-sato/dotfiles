2973fc253cdb9a13845f798ff197e0e025cf3ca4
Kei <keisto@amazon.com>
2020-05-23 11:38:11 +0000

    remove one character functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -177,26 +177,6 @@ function getcertnames() {
 	fi;
 }
 
-# `v` with no arguments opens the current directory in Vim, otherwise opens the
-# given location
-function v() {
-    if [ $# -eq 0 ]; then
-        vim .;
-    else
-        vim "$@";
-    fi;
-}
-
-# `o` with no arguments opens the current directory, otherwise opens the given
-# location
-function o() {
-    if [ $# -eq 0 ]; then
-        open .;
-    else
-        open "$@";
-    fi;
-}
-
 # `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
 # the `.git` directory, listing directories first. The output gets piped into
 # `less` with options to preserve color and line numbers, unless the output is
